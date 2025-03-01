c2721e41b787ebbb8ea93489e8069b54e02518f6
Seddon Shen <283481855@qq.com>
2025-03-01 15:22:58 +0800

    test: replace frontend assumption to Global for avoiding bugs

--- src/main/scala/nutcore/frontend/Frontend.scala
+++ src/main/scala/nutcore/frontend/Frontend.scala
@@ -132,12 +132,21 @@ class Frontend_inorder(implicit val p: NutCoreConfig) extends NutCoreModule with
       // assume(RVI.regImm(tmpInst) || RVI.loadStore(tmpInst))
       // assume(RVI.loadStore(tmpInst))
       // assume(RVI.loadStore(tmpInst))
-      assume(
+      // assume(
+      //   (hasCSR(tmpInst(31,20)) && (RVZicsr.reg(tmpInst) || RVZicsr.imm(tmpInst))) 
+      //   || 
+      //   (  RVI.regImm(tmpInst) || RVI.loadStore(tmpInst)  || RVI.other(tmpInst))
+      //   ||
+      //   (RVPrivileged.trap_return(tmpInst))
+      // )
+
+      BoringUtils.addSource(
         (hasCSR(tmpInst(31,20)) && (RVZicsr.reg(tmpInst) || RVZicsr.imm(tmpInst))) 
-        || 
+        ||
         (  RVI.regImm(tmpInst) || RVI.loadStore(tmpInst)  || RVI.other(tmpInst))
         ||
-        (RVPrivileged.trap_return(tmpInst))
+        (RVPrivileged.trap_return(tmpInst)),
+        "someassumeid"
       )
     }
   }
